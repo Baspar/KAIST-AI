2b2921b30964b54ed64e6015067f972ae7338beb
Blaine <bastien.laine@insa-rouen.fr>
2015-11-10 16:24:49 +0900

    add: plantuml UML generation for the graph

--- HW5/problem 2/genTree.sh
+++ HW5/problem 2/genTree.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+classId=0
+
 indent(){
     for i in $(seq $1);do echo -n "    "; done
 }
@@ -41,10 +43,14 @@ computeAverageEntropy(){
     echo $out
 }
 recc(){
-    local level="$1"
-    nb="$2"
-    list="$3"
+    local parentId="$1"
+    local parentSplit="$2"
+    local level="$3"
+    nb="$4"
+    list="$5"
 
+    local myClassId=$((classId+1))
+    let classId+=1
     total=$(echo "$list" | wc -l)
 
     nbOriginalZero=$(echo "$list"| sed "s/.*://g" | grep 0 | wc -l)
@@ -65,11 +71,14 @@ recc(){
 
         listZeroTmp=""
         listOneTmp=""
+        results=""
         for line in $(echo "$list" | tr '\n' ' ')
         do
             result=$(echo $line | sed "s/.*://g")
             data=$(echo $line | sed "s/:.*//g")
             criteria=$(echo $data | cut -d ',' -f $i)
+
+            results=$(echo -e "$results\n$result")
             if [ $criteria -eq 0 ]
             then
                 listZeroTmp=$(echo -e "$listZeroTmp\n$line")
@@ -100,22 +109,35 @@ recc(){
         fi
     done
 
-    local newNb=$(echo $nb | sed "s/$best//g; s/ +/ /g; s/^ //g; s/ $//g")
-    local newLevel=$((level+1))
+    if [ $(echo "$results" | sort | uniq | wc -l) -eq 3 ]
+    then
+        local newNb=$(echo $nb | sed "s/$best//g; s/ +/ /g; s/^ //g; s/ $//g")
+        local newLevel=$((level+1))
 
-    #indent $level
-    echo "Split I_$best"
+        echo -e "class \"Split on I_$best\" as $myClassId{\n$list\n}\n" >> graph.uml
+        if [ $parentId -ne 0 ];then echo "$parentId -- $myClassId : $parentSplit" >> graph.uml; fi
 
-    indent $level
-    echo -n "0:  "
-    if [ "$newNb" ]; then recc "$newLevel" "$newNb" "$listZero"; else echo ""; fi
 
-    indent $level
-    echo -n "1:  "
-    if [ "$newNb" ]; then recc "$newLevel" "$newNb" "$listOne"; else echo ""; fi
+        echo "Split I_$best"
+
+        indent $level
+        echo -n "0:  "
+        if [ "$listZero" ]; then recc "$myClassId" "0" "$newLevel" "$newNb" "$listZero"; else echo ""; fi
+
+        indent $level
+        echo -n "1:  "
+        if [ "$listOne" ]; then recc "$myClassId" "1" "$newLevel" "$newNb" "$listOne"; else echo ""; fi
+    else
+        echo ""
+        echo -e "class \"No Split needed\" as $myClassId{\n$list\n}\n" >> graph.uml
+        if [ $parentId -ne 0 ];then echo "$parentId -- $myClassId : $parentSplit" >> graph.uml; fi
+    fi
 }
 
 list=$(cat d | sed "s/ //g")
 nb=$(seq $(echo "$list"| head -n 1 | sed "s/[^,]//g" | wc -c))
 
-recc "0" "$nb" "$list"
+echo "@startuml" > graph.uml
+recc "0" "0" "0" "$nb" "$list"
+echo "hide empty methods" >> graph.uml
+echo "@enduml" >> graph.uml
