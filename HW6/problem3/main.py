44e0ad2a1d685dac09382d9b64a61c8a82f3dc6e
Blaine <bastien.laine@insa-rouen.fr>
2015-11-30 17:05:25 +0900

    fix: fix change

--- HW6/problem3/main.py
+++ HW6/problem3/main.py
@@ -1,6 +1,7 @@
-#/usr/bin/python2.7
+#!/usr/bin/python2
 from nltk.tokenize import TweetTokenizer
-from nltk.tokenize import PorterStemmer
+from nltk import PorterStemmer
+import re
 
 
 vocab={"UNKNOWN":0}
@@ -13,6 +14,17 @@ category_count_word={
     "ham":{"UNKNOWN":0}
 }
 
+# [ 3-A ]
+def replace_regexp(string):
+    #URLs
+    string=re.sub(r"(\w+://|[Ww]{3}\.)([A-Za-z0-9_-]+\.)+[A-Za-z]{2,5}(/[A-Za-z0-9_-]+)*/?", "<URL>", string)
+    #Phone
+    string=re.sub(r"\+?[0-9][0-9 -]{3,}[0-9]\+?", "<PHONE>", string)
+    #Prices TODO: add pounds and euro
+    string=re.sub(r"[$][0-9]+([,\.][0-9]+)?", "<PRICE>", string)
+
+    return string
+
 # [ 3-B ]
 def filter_stopwords(string, stopwords):
     tweet = TweetTokenizer()
@@ -20,7 +32,6 @@ def filter_stopwords(string, stopwords):
 
 # [ 3-C ]
 VOCMAX=10000
-
 # [ 3-D ]
 def do_stemming(string):
     stemmer = PorterStemmer()
@@ -28,19 +39,54 @@ def do_stemming(string):
 
 # [ 3-E ]
 def add_voc(label, string):
-    if(string in vocab.keys): #If we have already seen this word
+    #If we have already seen this word
+    if(string in vocab.keys()):
         vocab[string]+=1
         category_count_word[label][string]=category_count_word[label].get(string, 0)+1
-    elif(vocab.size<=VOCMAX):#New word, doesn't exceed the max size of the voc
+    #New word, doesn't exceed the max size of the voc
+    elif(len(vocab)<=VOCMAX):
         vocab[string]=1
         category_count_word[label][string]=1
-    else:#We exceed the max size of the voc
+    #We exceed the max size of the voc
+    else:
         vocab["UNKNOWN"]+=1
         category_count_word[label]["UNKNOWN"]+=1
 
 def train(labels, strings):
-    for label, string in labels, strings:
+    for (label, string) in zip(labels, strings):
         category_count[label]+=1
         for word in string:
             add_voc(label, word)
     return 0
+
+def complete_train():
+    #Loading stopWords
+    stopwords=[]
+    print("Loading stopwords...")
+    with open("stopwords.txt") as stopwords_file:
+        for word in stopwords_file.readline().split(','):
+            stopwords.append(word[1:-1])
+    print("Stopwords loaded")
+
+
+
+    print("Loading and preprocess training cases...")
+    labels=[]
+    strings=[]
+    with open("train") as train_file:
+        for line in train_file:
+            [label, string]=line.split('|', 1)
+
+            raw_string=replace_regexp(string)
+            list_word=filter_stopwords(raw_string, stopwords)
+            stemmed_word=do_stemming(list_word)
+
+            labels.append(label)
+            strings.append(stemmed_word)
+    print("Training cases loaded and preprocessed")
+
+    print("Training knowledge base...")
+    train(labels, strings)
+    print("Knowledge base trained")
+
+complete_train()
