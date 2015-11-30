9bfb708a65cb785d958d00244c46421bd113e071
Blaine <bastien.laine@insa-rouen.fr>
2015-11-30 13:13:41 +0900

    add: 3-D

--- HW6/problem3/main.py
+++ HW6/problem3/main.py
@@ -1,6 +1,13 @@
 #/usr/bin/python2.7
 from nltk.tokenize import TweetTokenizer
+from nltk.tokenize import PorterStemmer
 
 # [ 3-B ]
 def filter_stopwords(string, stopwords):
-    return [x for x in TweetTokenizer().tokenize(string) if x not in stopwords]
+    tweet = TweetTokenizer()
+    return [x for x in tweet.tokenize(string) if x not in stopwords]
+
+# [ 3-D ]
+def do_stemming(string):
+    stemmer = PorterStemmer()
+    return [stemmer.stem(word) for word in string]
